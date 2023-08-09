5671b3e9807868ecaf031469fb6328ad47810730
afaysalk <afaysalkhaldi@gmail.com>
2023-08-09 15:08:40 +0100

    ✨ Generator function working, definitions done

--- geo_heatmap.py
+++ geo_heatmap.py
@@ -148,3 +148,171 @@ def gridder(min_hour,max_hour,n,m):
         colormap.caption = 'Relative density of fleet activity per cell'
         colormap.add_to(m)
         return m
+
+class Generator:
+    def __init__(self):
+        self.coordinates = collections.defaultdict(int)
+        self.max_coordinates = (0, 0)
+        self.max_magnitude = 0
+        self.resetStats()
+
+    def resetStats(self):
+        self.stats = {
+            "Data points": 0
+        }
+
+    @staticmethod
+    
+
+
+    
+
+    def loadKMLData(self, file_name, date_range):
+        """Loads the Google location data from the given KML file.
+
+        Arguments:
+            file_name {string or file} -- The name of the KML file
+                (or an open file-like object) with the Google location data.
+            date_range {tuple} -- A tuple containing the min-date and max-date.
+                e.g.: (None, None), (None, '2019-01-01'), ('2017-02-11'), ('2019-01-01')
+        """
+        xmldoc = minidom.parse(file_name)
+        gxtrack = xmldoc.getElementsByTagName("gx:coord")
+        when = xmldoc.getElementsByTagName("when")
+        w = [Bar(), Percentage(), " ", ETA()]
+
+        with ProgressBar(max_value=len(gxtrack), widgets=w) as pb:
+            for i, number in enumerate(gxtrack):
+                loc = (number.firstChild.data).split()
+                coords = (round(float(loc[1]), 6), round(float(loc[0]), 6))
+                date = when[i].firstChild.data
+                if dateInRange(date[:10], date_range):
+                    self.updateCoord(coords)
+                pb.update(i)
+
+    def loadGPXData(self, file_name, date_range):
+        """Loads location data from the given GPX file.
+
+        Arguments:
+            file_name {string or file} -- The name of the GPX file
+                (or an open file-like object) with the GPX data.
+            date_range {tuple} -- A tuple containing the min-date and max-date.
+                e.g.: (None, None), (None, '2019-01-01'), ('2017-02-11'), ('2019-01-01')
+        """
+        xmldoc = minidom.parse(file_name)
+        gxtrack = xmldoc.getElementsByTagName("trkpt")
+        w = [Bar(), Percentage(), " ", ETA()]
+
+        with ProgressBar(max_value=len(gxtrack), widgets=w) as pb:
+            for i, trkpt in enumerate(gxtrack):
+                lat = trkpt.getAttribute("lat")
+                lon = trkpt.getAttribute("lon")
+                coords = (round(float(lat), 6), round(float(lon), 6))
+                date = trkpt.getElementsByTagName("time")[0].firstChild.data
+                if dateInRange(date[:10], date_range):
+                    self.updateCoord(coords)
+                pb.update(i)
+
+    
+
+
+
+    
+
+
+
+
+
+    def updateCoord(self, coords):
+        self.coordinates[coords] += 1
+        self.stats["Data points"] += 1
+        if self.coordinates[coords] > self.max_magnitude:
+            self.max_coordinates = coords
+            self.max_magnitude = self.coordinates[coords]
+
+    def generateMap(self, settings):
+        """Generates the heatmap.
+
+        Arguments:
+            settings {dict} -- The settings for the heatmap.
+
+        Returns:
+            Map -- The Heatmap.
+        """
+        tiles = settings["tiles"]
+        zoom_start = settings["zoom_start"]
+        radius = settings["radius"]
+        blur = settings["blur"]
+        min_opacity = settings["min_opacity"]
+        max_zoom = settings["max_zoom"]
+
+        map_data = [(coords[0], coords[1], magnitude)
+                    for coords, magnitude in self.coordinates.items()]
+
+       
+       # Generate map
+        m = folium.Map(location=self.max_coordinates,
+                        zoom_start=zoom_start,
+                        tiles=tiles,
+                        attr="<a href=https://github.com/luka1199/geo-heatmap>geo-heatmap</a>")
+
+            # Generate heat map
+        heatmap = HeatMap(map_data,
+                            max_val=self.max_magnitude,
+                            min_opacity=min_opacity,
+                            radius=radius,
+                            blur=blur,
+                            max_zoom=max_zoom)
+
+        
+        # limiting time window for our data to 8 am - 5 pm and also grid is 20 x 20 
+        m=gridder(0,24,20,m)
+        m.add_child(heatmap)
+
+        
+        return m
+
+
+    
+    
+
+    def run(self, data_files, output_file, date_range, stream_data, settings):
+        """Load the data, generate the heatmap and save it.
+
+        Arguments:
+            data_files {list} -- List of names of the data files with the Google
+                location data or the Google takeout ZIP archive.
+            output_file {string} -- The name of the output file.
+            date_range {tuple} -- A tuple containing the min-date and max-date.
+                e.g.: (None, None), (None, '2019-01-01'), ('2017-02-11'), ('2019-01-01')
+            stream_data {bool} -- Stream option.
+            settings {dict} -- The settings for the heatmap.
+        """
+        self.resetStats()
+
+        for i, data_file in enumerate(data_files):
+            print("\n({}/{}) Loading data from {}".format(
+                i + 1,
+                len(data_files) + 2,
+                data_file))
+            
+            if data_file.endswith(".gpx"):
+                self.loadGPXData(data_file, date_range)
+            else:
+                raise NotImplementedError(
+                    "Unsupported file extension for {!r}".format(data_file))
+
+        print("\n({}/{}) Generating heatmap".format(
+            len(data_files) + 1,
+            len(data_files) + 2))
+        m = self.generateMap(settings)
+        print("\n({}/{}) Saving map to {}\n".format(
+            len(data_files) + 2,
+            len(data_files) + 2,
+            output_file))
+        m.save(output_file)
+
+        print("Stats:")
+        for name, stat in self.stats.items():
+            print("{}: {}".format(name, stat))
+        print()
